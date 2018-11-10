4abb658d2bfd01ba9f4409a6d7ad99a5c35f264c
Steven Gong <steven.gong@iag.com.au>
2018-11-10 21:17:55 +1100

    pre order traversal tree

--- test/rules/twoSum.test.js
+++
@@ -1,8 +0,0 @@
-import twoSum from '../../src/rules/twoSum/twoSum';
-import { nums, target } from '../mock/twoSumsData';
-
-test('twoSum rule should return the first match', () => {
-  const result = twoSum(nums, target);
-  expect(result[0]).toBe(0);
-  expect(result[1]).toBe(1);
-});
